bb5a5fb59df8d82c0df58264018b793a8be0168f
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-09-26 18:46:50 -0400

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
-		let value = `${result[0]['body']} ${result[1]['firstName']} ${result[1]['lastName']}`
+      const value = `${result[0].body} ${result[1].firstName} ${result[1].lastName}`;
       console.log(value);
     })
     .catch(() => {
-		console.log("Signup system offline")
-	});
+      console.log('Signup system offline');
+    });
 }

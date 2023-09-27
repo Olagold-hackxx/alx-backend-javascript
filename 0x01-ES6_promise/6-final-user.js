67df10ad1fd661d2a48066b511f222b4b967765c
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-09-26 21:16:28 -0400

    task 8

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,22 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((value) => {
-      const result = [];
-      value.forEach((res) => {
-        result.push({
-          status: res.status,
-          value: res.value ? res.value : res.reason,
-        });
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((value) => {
+    const result = [];
+    value.forEach((res) => {
+      result.push({
+        status: res.status,
+        value: res.value ? res.value : res.reason,
       });
-      return result;
     });
+    return result;
+  });
 }

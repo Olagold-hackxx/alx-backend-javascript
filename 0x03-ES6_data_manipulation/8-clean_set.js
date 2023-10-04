0ebbaadaddd9e2fc8ed0f922b50aaced82e09aa3
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-03 22:56:10 -0400

    Data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (!startString) { return ''; }
   const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
   const cleanedValues = filteredValues.map((value) => value.substring(startString.length));
   const resultString = cleanedValues.join('-');

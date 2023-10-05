88d1bb0a41e21dbcfc6fbab917bcf69f6b5e3a7a
Aileru Abdulqoyyum <aileruenterprise@gmail.com>
2023-10-04 20:21:51 -0400

    Typescript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,24 @@
+interface Student {
+	firstName: string,
+	lastName: string,
+	age: number,
+	location: string
+}
+
+const alxStudent: Student = {
+	firstName: 'Abdul',
+	lastName: 'Aileru',
+	age: 19,
+	location: 'Nigeria'
+}
+
+const holbertonStudent: Student = {
+	firstName: 'Julien',
+	lastName: 'Barber',
+	age: 30,
+	location: 'United States'
+}
+
+const studentsList: Student[] = [
+	alxStudent, holbertonStudent
+]
